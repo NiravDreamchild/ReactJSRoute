dde00e8b4e8dfbb99d009eac7cdcbe6e07f82170
NiravDreamchild <nirav.dreamchild@gmail.com>
2024-12-04 16:18:57 +0530

    Minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import HeaderBar from "./components/HeaderBar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <HeaderBar />
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
